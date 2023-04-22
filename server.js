c9a446ce87ad3763147c8d46debbfb1c48f7af33
nabesteron <49988463+nabesteron@users.noreply.github.com>
2023-04-22 16:35:45 -0500

    changed the port number to 3001

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const port = 3000; // Use the default port 3000
+const port = 3001; // Use the default port 3000
 
 const books = [
   {
